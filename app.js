ed50128749d81433d7588d45f2763d42a3bef4ea
Guilherme Lessa <guilessa@users.noreply.github.com>
2017-12-03 18:28:48 -0200

    Adicionando o ejs com view engine do express e criando algumas views para desenvolvimento do projeto

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 
+// Troca a engine do express para utilizar o EJS nas views
+app.set('view engine', 'ejs');
+
 // Cria às páginas
 app.get('/', function(req, res) {
   res.send("<html><body>HTML</body></html>")
 });
 
 app.get('/moda', function(req, res) {
-  res.send("<html><body>MODA</body></html>")
+  // Renderiza as views
+  res.render('secao/moda')
 });
 
 // Sobe o server na porta 3000
